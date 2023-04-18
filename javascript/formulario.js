7ca0eb1518be0cddce26aa779a757d3acdb85ea9
Alvaro Bermudez Gamez <alvarobermudezgamez@uma.es>
2023-04-18 08:51:17 +0200

    refactor formulario

--- javascript/formulario.js
+++ javascript/formulario.js
@@ -15,23 +15,33 @@
       form.classList.add('was-validated')
     }, false)
 
-
   })
 
 })()
 
+const pwdError = document.getElementById("pwdError")
 const pwdRepError = document.getElementById("pwdRepError") 
 const contraseña = document.getElementById("contraseña")
 const repetirContraseña = document.getElementById("repitaContraseña")
 
-repetirContraseña.addEventListener('submit', ()=>{
+
+repetirContraseña.onkeyup = function(){
   if(repetirContraseña.value != contraseña.value){
     pwdRepError.style.display = "initial"
   }else{
     pwdRepError.style.display = "none"
   }
+}
 
-})
+contraseña.onkeyup = function(){
+  if(contraseña.value.length<6){
+    pwdError.style.display = "initial"
+  }else{
+    pwdError.style.display = "none"
+  }
+}
+
+
 
 
 
